93309b60a02c5adbfaa623788caf1a5f29b887a0
Kyle <prophet@hanyang.ac.kr>
2023-06-02 18:18:29 +0900

    Image Path Selector Debug

--- src/main/webapp/js/showImage.js
+++ src/main/webapp/js/showImage.js
@@ -1,22 +1,19 @@
 'use strict'
 
 // 이미지 원본 보기
-function showImage() {
-    // alert(fileCallPath)
-    let fileCallPath = document.querySelector("#products > div:nth-child(2) > img").getAttribute('src')
-    console.log(fileCallPath)
+function showImage(image) {
+    let fileCallPath = image.target.getAttribute('src')
 
     $(".bigPictureWrapper").css("display", "flex").show()
 
     $(".bigPicture")
-        // .html("<img src='/display?fileName=" + encodeURI(fileCallPath) + "'>")
-        .html("<img src='" + encodeURI(fileCallPath) + "'>")
+        .html(`<img src='${encodeURI(fileCallPath)}'>`)
         .animate({width: '100%', height: '100%'}, 1000)
 }
 
 $(() => {
     // 원본 이미지 확대
-    $(".product").on("click", showImage)
+    $(".product").click(showImage)
 
     // 원본 이미지 닫기
     $(".bigPictureWrapper").on("click", () => {
